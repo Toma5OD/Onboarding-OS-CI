c59e5a02e1f30519ea6f83697302e35a47afea57
Tomás Ó Dálaigh <tom1996.18@gmail.com>
2023-08-28 12:28:10 +0100

    Comments added for gnu sort

--- gnu-sort/gnu-sort-text.go
+++ gnu-sort/gnu-sort-text.go
@@ -3,29 +3,36 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os"
-	"sort"
+	"os"   // For operating system-level functionalities like file handling
+	"sort" // For sorting functionalities
 )
 
-func sortMain() {
-	// Read lines from a file
+func main() {
+	// Open the file "input.txt" in read-only mode
 	file, _ := os.Open("input.txt")
+	// Ensure the file is closed when the function exits
 	defer file.Close()
 
+	// Declare a slice to hold the lines from the file
 	var lines []string
+	// Create a scanner to read the file line by line
 	scanner := bufio.NewScanner(file)
+	// Scan each line and append it to the 'lines' slice
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
 
-	// Sort lines
+	// Sort the 'lines' slice in ascending alphabetical order
 	sort.Strings(lines)
 
-	// Write sorted lines to a new file
+	// Create a new file named "sorted.txt" in write-only mode
 	outFile, _ := os.Create("sorted.txt")
+	// Create a buffered writer for efficient file writing
 	writer := bufio.NewWriter(outFile)
+	// Write each sorted line to the new file
 	for _, line := range lines {
 		fmt.Fprintln(writer, line)
 	}
+	// Flush the writer buffer to make sure all lines are written to the file
 	writer.Flush()
 }
